44c917003270a1c29f1f7b876d2d5adc6790d3d3
Dan Gautsch <dgautsch@gmail.com>
2018-08-04 23:51:52 -0400

    Adding bootstrap and setting content in connectedCallback

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -1,31 +1,53 @@
 customElements.define(
-  "modal-dialog",
+  'modal-dialog',
   class extends HTMLElement {
     get template() {
-        return `
+      return `
         <link rel="stylesheet" href="./components/modal/modal.css">
-        <div class="modal-backdrop no-scroll active">
+        <div class="modal-backdrop no-scroll">
             <div tabindex="0"></div>
             <div id="alert_modal" role="alertdialog" aria-modal="true" aria-labelledby="modal_label" aria-describedby="modal_desc"
                 class="default_modal">
                 <h2 id="modal_label" class="modal_label">Title</h2>
-                <div id="modal_desc" class="modal_desc">
-                    <p>Content</p>
-                </div>
+                <p id="modal_desc" class="modal_desc">
+                    Content
+                </p>
                 <div class="modal_form_actions">
-                    <button type="button">Close</button>
-                    <button type="button">Confirm</button>
+                    <button type="button" class="btn btn-primary">Close</button>
+                    <button type="button" class="btn">Confirm</button>
                 </div>
             </div>
             <div tabindex="0"></div>
-        </div>`
+        </div>`;
     }
+
     constructor() {
       super();
       const template = document.createElement('template');
-      const shadow = this.attachShadow({ mode: "open" });
+      const shadow = this.attachShadow({ mode: 'open' });
       template.innerHTML = this.template;
       shadow.appendChild(template.content.cloneNode(true));
+    }
+
+    get attrTitle() {
+        return this.getAttribute('title');
+    }
+
+    get content() {
+        return this.innerHTML;
+    }
+
+    set setModalLabel(title) {
+        this.shadowRoot.getElementById('modal_label').innerHTML = title;
+    }
+
+    set setModalDesc(desc) {
+        this.shadowRoot.getElementById('modal_desc').innerHTML = desc;
+    }
+
+    connectedCallback() {
+        this.setModalLabel = this.attrTitle;
+        this.setModalDesc = this.content;
     }
   }
 );
