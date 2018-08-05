4275c64d0da15e147811e6f2a20482f03d9d5ecb
Dan Gautsch <dgautsch@urbn.com>
2018-08-05 09:48:35 -0400

    Converting to shadow dom and writing hide/show logic

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -3,71 +3,61 @@ customElements.define(
   class extends HTMLElement {
     get template() {
       return `
-        <div class="modal fade">
+      <link rel="stylesheet" href="./components/modal/modal.css">
+      <div id="modal-container">
+        <div class="modal fade" id="modal">
             <div tabindex="0"></div>
-            <div id="alert_modal" role="alertdialog" aria-modal="true" aria-labelledby="modal_label" aria-describedby="modal_desc"
-                class="modal-dialog">
+            <div role="alertdialog" aria-modal="true" class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h2 id="modal_label" class="modal_label"><slot name="modal-label">Title</slot></h2>
+                    <h2 class="modal-title"><slot name="modal-title">Default Title</slot></h2>
                   </div>
-                  <div id="modal_desc" class="modal-body">
-                      <slot name="modal-description">Content</slot>
+                  <div class="modal-body">
+                      <slot name="modal-body">Default Body Contebt</slot>
                   </div>
                   <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-secondary" id="modal-close">Close</button>
                       <button type="button" class="btn btn-primary">Confirm</button>
                   </div>
                 </div>
             </div>
             <div tabindex="0"></div>
-        </div>`;
+        </div>
+        <!-- Backdrop -->
+        <div class="modal-backdrop"></div>
+      </div>
+
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" id="modal-trigger">
+        <slot name="modal-trigger">Open</slot>
+      </button>
+      `;
     }
 
     constructor() {
       super();
       const template = document.createElement('template');
-      const shadow = this.attachShadow({ mode: 'open' });
       template.innerHTML = this.template;
-      shadow.appendChild(template.content.cloneNode(true));
-      // this.appendChild(template.content.cloneNode(true));
+      this.shadow = this.attachShadow({ mode: 'open' });
+      this.shadow.appendChild(template.content.cloneNode(true));
+
+      //register click event
+      this.shadow.getElementById('modal-trigger').onclick = this.open.bind(this);
+      this.shadow.getElementById('modal-close').onclick = this.close.bind(this);
     }
 
-    getSetting(attr) {
-      if (!this.shadowRoot.host.hasAttribute(attr)) {
-        throw 'Attribute not set for ' + attr;
-      }
-      return this.shadowRoot.host.getAttribute(attr);
+    open() {
+      const modal = this.shadow.querySelector('#modal');
+      modal.dispatchEvent(new Event('modal-open', {bubbles: true, composed: true}));
+      this.shadow.querySelector('.modal-backdrop').classList.add('show');
+      this.shadow.querySelector('#modal-container').classList.add('modal-open');
     }
 
-    get id() {
-      return this.getSetting('modalId');
-    }
-
-    get title() {
-      return this.getSetting('title');
-    }
-
-    get content() {
-      return this.getSetting('content');
-    }
-
-    set setModalTitle(title) {
-      this.shadowRoot.getElementById('modal_label').innerHTML = title;
-    }
-
-    set setModalContent(desc) {
-      this.shadowRoot.getElementById('modal_desc').innerHTML = desc;
-    }
-
-    set setModalId(id) {
-      this.shadowRoot.querySelector('.modal').setAttribute('id', id);
-    }
-
-    connectedCallback() {
-      this.setModalTitle = this.title;
-      this.setModalContent = this.content;
-      this.setModalId = this.id;
+    close() {
+      const modal = this.shadow.querySelector('#modal');
+      modal.dispatchEvent(new Event('modal-close', {bubbles: true, composed: true}));
+      this.shadow.querySelector('.modal-backdrop').classList.remove('show');
+      this.shadow.querySelector('#modal-container').classList.remove('modal-open');
     }
   }
 );
