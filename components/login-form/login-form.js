4cb691cab060ef2ecb092a6aaa91b2c86029de6c
Dan Gautsch <dgautsch@urbn.com>
2018-08-07 20:45:06 -0400

    Adding login-form component and global styles

---
+++ components/login-form/login-form.js
@@ -0,0 +1,34 @@
+customElements.define(
+    'login-form',
+    class extends HTMLElement {
+      get template() {
+        return `
+        <link rel="stylesheet" href="./components/login-form/login-form.css">
+        <form>
+            <fieldset>
+                <legend>Login Form</legend>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" aria-describedby="emailHelp" placeholder="Enter email" />
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" />
+                </div>
+                <button type="submit">Submit</button>
+            </fieldset>
+        </form>
+        `;
+      }
+  
+      constructor() {
+        super();
+        const template = document.createElement('template');
+        template.innerHTML = this.template;
+        this.shadow = this.attachShadow({ mode: 'open' });
+        this.shadow.appendChild(template.content.cloneNode(true));
+      }
+    }
+  );
+  
